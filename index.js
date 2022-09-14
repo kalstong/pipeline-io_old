e782198243c06af10aecf5de96399c12cbae51ec
Ricardo Silva <github@ricardosilva.pt>
2022-09-15 00:14:20 +0100

    Parse arguments Load and parse pipeline from json

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 const path = require('path')
-const { log } = require(path.join(__dirname, 'lib'))
+const { log, utils } = require(path.join(__dirname, 'lib'))
 const package = require(path.join(__dirname, 'package.json'))
 
+
+process.title = `pipeline-io`
+
+log('boot').info(`Welcome to pipeline-io v.${package.version}!\nFor more information please visit ${package.homepage}`)
+
+// Arguments
+let arguments = {}
 try {
-    process.title = `pipeline-io`
+    arguments = utils.parseArgv(process.argv, { 'pipeline': 'pipeline' }, 2)
+} catch (_) {
+    log('boot').warn('Failed to parse process arguments.')
+}  
 
-    log('boot').info(`Welcome to pipeline-io v.${package.version}!\nFor more information please visit ${package.homepage}`)
+try {
+    if (!arguments.pipeline) {
+        log('pipeline').warn('No pipeline specified. Nothing to do, program will stop.')
+        process.exit(0)
+    }
 
+    let pipelineFile = utils.openFileSync(arguments.pipeline.value)
+    
+    if (!pipelineFile) {
+        log('boot').warn('Pipeline specified cannot be loaded. Nothing to do, program will stop.')
+        process.exit(0)
+    }
+
+    let pipeline = JSON.parse(pipelineFile)
+
+    // Parse pipeline
+    // Run setup
+    
 } catch (err) {
+    log('pipeline').error('Critical error. Program will stop.', err)
     process.exit(1)
 }
 
 process.on('unhandledRejection', function (err, promise) {
+    log('pipeline').error('Critical error. Unhandled Rejection detected. Program will stop.', err)
     process.exit(1)
 });
